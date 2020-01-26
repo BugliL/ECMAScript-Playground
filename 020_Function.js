9f69930d1e1791d7b121e08e3a0cb457d77fbf0c
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2020-01-26 19:43:50 +0100

    Introduced Rest parameters

--- 020_Function.js
+++ 020_Function.js
@@ -1,7 +1,7 @@
 /*
     functions
 
-    BNF:
+    BNF: <function_declaration> ::=
     function <identifier>(<declaration_list>){
         <statements>
     }
@@ -16,3 +16,17 @@ function foo(a, b=23, c=42){
 console.log(foo(2));           // 1932
 console.log(foo(2, 3));        // 252
 console.log(foo(2, 3, 4));     // 24
+
+/*
+    Rest parameters
+    A rest parameter is indicated with 3 dots.
+    It is an Array containing all parameters passed from that point until the
+    end in the function call
+
+    <rest_param> ::= ...<identifier>
+*/
+function bar(a, ...b){
+    const sum = b.reduce((x, y) => x + y)
+    return a*sum;
+}
+console.log(bar(2,4,5,6,7));   // 44
